0c11d66d636034158c073546cecc46eed659e26c
lowtierre <lorenzoterreni1997@gmail.com>
2023-08-22 10:55:10 +0200

    make mobile slower

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ let frames;
 const isTouchDevice = () => {
     try {
         document.createEvent("TouchEvent");
-        frames = 50;
+        frames = 150;
     } catch (e) {
         frames = 100;
     }
